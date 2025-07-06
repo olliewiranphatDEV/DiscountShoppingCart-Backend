12a204de6a8daa4468dddeb959603b28c68e3524
Wiranphat Pattaramool <olliewiranphat@gmail.com>
2025-07-07 02:19:48 +0700

    Update quantity API

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -18,6 +18,9 @@ exports.getCartsByUserID = TryCatch(async (req, res) => {
                     product: true
                 }
             }
+        },
+        orderBy: {
+            cartID: "asc"
         }
     });
     console.log("cart, results", results);
@@ -45,6 +48,55 @@ exports.deleteCartIDByUserID = TryCatch(async (req, res) => {
 
     res.status(200).json({ message: `Success delete cartID ${req.params.cartID} of userID ${req.user.userID} ` })
 })
+
+
+exports.updateCartIDQuantity = TryCatch(async (req, res) => {
+    const { cartID, productID, quantity } = req.body;
+
+
+    if (!cartID || !productID || quantity < 1) {
+        return createError(400, "Invalid cart data");
+    }
+
+    console.log('cartID >>', cartID);
+    console.log('productID >>', productID);
+    console.log("quantity >>", quantity);
+
+
+    // CALCULATE NEW-totalPriceItem = quantity * product.price
+    const product = await prisma.product.findUnique({
+        where: { productID: productID }
+    });
+
+    if (!product) {
+        return createError(404, `No have this productID ${productID}`);
+    }
+    console.log('product.price >>', parseInt(product.price));
+    const newTotalPriceItem = parseInt(product.price) * quantity;
+    console.log('newTotalPriceItem >>', newTotalPriceItem);
+
+    // UPDATE quantity INTO ProductOnCart
+    const updateQuantity = await prisma.productOnCart.update({
+        where: {
+            productID_cartID: {
+                productID,
+                cartID
+            }
+        },
+        data: { quantity }
+    });
+    console.log('updateQuantity >>', updateQuantity);
+
+    // UPDATE totalPriceItem INTO Cart
+    const updateTotalPriceItem = await prisma.cart.update({
+        where: { cartID },
+        data: { totalPriceItem: newTotalPriceItem }
+    });
+    console.log('updateTotalPriceItem >>', updateTotalPriceItem);
+
+    res.status(200).json({ message: "Cart updated successfully", results: { updateQuantity, updateTotalPriceItem } });
+});
+
 
 
 
--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { getCartsByUserID, deleteCartIDByUserID } = require('../controllers/cartController');
+const { getCartsByUserID, deleteCartIDByUserID, updateCartIDQuantity, } = require('../controllers/cartController');
 const authorization = require('../middlewares/authorization');
 const cartRouter = express.Router();
 
 
 cartRouter.get('/my-carts', authorization, getCartsByUserID);
 cartRouter.delete('/delete/:cartID', authorization, deleteCartIDByUserID);
-// cartRouter.patch('/cart/update-quantity', authorization, updateQuantity) 
+cartRouter.patch('/update-quantity', authorization, updateCartIDQuantity)
 
 
 module.exports = cartRouter;
